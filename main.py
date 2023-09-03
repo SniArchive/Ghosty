fbb7df645845990e78c2a4187baa52e81717aeb7
snipc <126542340+NotSnipc@users.noreply.github.com>
2023-09-03 13:24:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from src.Features.speak import speak
+from src.Features.startup import wishme
+from src.Features.speechreco import takeSpeech
+import time
+
+# wishme() #for wishing
+takeSpeech()
+
+
+
+
+
+
+
